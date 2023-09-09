4f2380e28b19beb0b04416de6621fc534c43e535
michelledli <limichelle.d@gmail.com>
2023-09-08 19:11:25 -0700

    initial commit

---
+++ farmfolio-api/src/main/java/com/iloveyou/farmfolioapi/FarmfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.iloveyou.farmfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmfolioApiApplication.class, args);
+	}
+
+}
---
+++ farmfolio-api/src/test/java/com/iloveyou/farmfolioapi/FarmfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iloveyou.farmfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
